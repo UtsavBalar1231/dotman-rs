92cc921f7d1245e36e2f223f3df056256430711f
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-06-09 11:37:57 +0530

    scripts: setup_env: Install only required package
    Change-Id: I8cb72c3ae66358ddbc77609230816b9948a341fd
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -14,19 +14,30 @@ if [ -f ~/.config/zshrc ]; then
 	mv ~/.config/zshrc ~/.zshrc
 fi
 
+debian_packages="gnupg flex bc bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libncurses5 lib32ncurses-dev libssl-dev libelf-dev dwarves libxml2-utils xsltproc unzip rsync python3 python-is-python3 ripgrep exa curl wget nodejs npm kitty luarocks shellcheck"
+
+arch_packages="gnupg flex bison bc binutils bzip2 ed gcc-libs grep gzip inetutils libarchive libelf libtool linux-headers make pacman patch pkgconf sed sudo systemd systemd-libs texinfo util-linux which xz exa curl wget nodejs npm clang llvm llvm-libs kitty luarocks shellcheck"
+
 if [ ! -f /etc/arch-release ]; then
-	sudo apt update
-	sudo apt install -y \
-		gnupg flex bc bison build-essential zip curl zlib1g-dev \
-		gcc-multilib g++-multilib libncurses5 lib32ncurses-dev libssl-dev libelf-dev \
-		dwarves libxml2-utils xsltproc unzip rsync python3 python-is-python3 \
-		ripgrep exa curl wget nodejs npm kitty luarocks shellcheck
+	sudo apt update -y
+
+	IFS=' ' read -r -a packages <<< "$debian_packages"
+	for package in "${packages[@]}"; do
+		if ! dpkg -s "$package" >/dev/null 2>&1; then
+			sudo apt install -y "$package"
+		else
+			echo "$package is already installed"
+		fi
+	done
 else
 	sudo pacman -Syu --noconfirm
-	sudo pacman -S --noconfirm \
-		gnupg flex bison bc binutils bzip2 \
-		ed gcc-libs grep gzip inetutils libarchive libelf libtool \
-		linux-headers make pacman patch pkgconf sed sudo systemd \
-		systemd-libs texinfo util-linux which xz exa curl wget \
-		nodejs npm clang llvm llvm-libs kitty luarocks shellcheck
+
+	IFS=' ' read -r -a packages <<< "$arch_packages"
+	for package in "${packages[@]}"; do
+		if ! pacman -Qi "$package" >/dev/null 2>&1; then
+			sudo pacman -S --noconfirm "$package"
+		else
+			echo "$package is already installed"
+		fi
+	done
 fi
