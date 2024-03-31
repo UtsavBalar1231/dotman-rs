093eb9a1f54550f5cbceb22e9c2aae8ab9ad5a65
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-03-31 19:18:25 +0530

    configs: nvim: Redo none-ls configuration
    Change-Id: Id47749aaac718575b97d5bf9e9d7671737b056de
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- configs/nvim/after/plugin/none-ls.lua
+++ configs/nvim/after/plugin/none-ls.lua
@@ -9,21 +9,20 @@ null_ls.setup({
 	sources = {
 		-- Code Actions
 		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.code_actions.shellcheck,
 
 		-- Completions
 		null_ls.builtins.completion.luasnip,
 		null_ls.builtins.completion.spell,
-		null_ls.builtins.completion.spell,
 		null_ls.builtins.completion.tags,
 
 		-- Diagnostics
-		null_ls.builtins.diagnostics.clang_check,
-		-- null_ls.builtins.diagnostics.codespell.with({
-		-- 	Filetypes = { "markdown", "text" },
-		-- }),
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.diagnostics.checkmake,
+		null_ls.builtins.diagnostics.commitlint,
+		null_ls.builtins.diagnostics.gitlint,
+		null_ls.builtins.diagnostics.markdownlint,
+		null_ls.builtins.diagnostics.pylint,
+		null_ls.builtins.diagnostics.rstcheck,
+		null_ls.builtins.diagnostics.stylelint,
 		null_ls.builtins.diagnostics.todo_comments,
 		null_ls.builtins.diagnostics.trail_space,
 		null_ls.builtins.diagnostics.zsh,
@@ -31,20 +30,12 @@ null_ls.setup({
 		-- Formatting
 		null_ls.builtins.formatting.blackd,
 		null_ls.builtins.formatting.clang_format,
-		null_ls.builtins.formatting.dprint,
-		null_ls.builtins.formatting.eslint_d,
 		null_ls.builtins.formatting.gofmt,
-		null_ls.builtins.formatting.jq,
 		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.rustfmt,
-		null_ls.builtins.formatting.shellharden,
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.formatting.stylelint,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.taplo,
-		null_ls.builtins.formatting.trim_newlines,
-		null_ls.builtins.formatting.trim_whitespace,
 		null_ls.builtins.formatting.yamlfmt,
 
 		-- Hover options
